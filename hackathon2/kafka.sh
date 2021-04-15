f606dc36109dc38cc83b10a43006eb70439eebb0
Varun Nambigari <varun.nambigari@students.iiit.ac.in>
2021-04-15 15:14:16 +0000

    Update kafka.sh

---
+++ hackathon2/kafka.sh
@@ -0,0 +1,11 @@
+cd /opt/
+
+wget https://downloads.apache.org/kafka/2.7.0/kafka_2.13-2.7.0.tgz
+
+tar -xvzf kafka_2.13-2.7.0.tgz
+
+cd kafka_2.13-2.7.0
+
+bin/zookeeper-server-start.sh config/zookeeper.properties
+
+JMX_PORT=8004 bin/kafka-server-start.sh config/server.properties
