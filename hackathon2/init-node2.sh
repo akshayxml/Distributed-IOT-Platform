802c9d33130d0110ab3c264b15865ff8df319062
Varun Nambigari <varun.nambigari@students.iiit.ac.in>
2021-04-17 11:44:56 +0000

    run container in node 2

--- hackathon2/init-node2.sh
+++ hackathon2/init-node2.sh
@@ -1,2 +1,3 @@
-
 ssh IAS-Node-2@52.146.1.189 sudo docker build -t plat-man -f /home/IAS-Node-2/iot_platform/platformManager_docker/dockerfile /home/IAS-Node-2/iot_platform/platformManager_docker/
+
+ssh IAS-Node-2@52.146.1.189 sudo docker run -p 5001:5001 -e KAFKA_ADDRESS="52.146.2.26" plat-man
