282990b250fa516f7b23fa71e53dc655bab0ff50
varunnambi <varun.nambigari@students.iiit.ac.in>
2021-05-04 23:51:52 +0530

    updated kafka.sh file. Checks if folder already installed

--- hackathon2/kafka.sh
+++ hackathon2/kafka.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
+fuser -k 9092/tcp # it will kill if any previous zookeeper was running
+fuser -k 2181/tcp
+
 cd /opt/
 
-wget https://downloads.apache.org/kafka/2.7.0/kafka_2.13-2.7.0.tgz
+if [[ ! -d "/opt/kafka_2.13-2.7.0" ]]
+then
+    wget https://downloads.apache.org/kafka/2.7.0/kafka_2.13-2.7.0.tgz
 
-tar -xvzf kafka_2.13-2.7.0.tgz
+    tar -xvzf kafka_2.13-2.7.0.tgz
+fi
 
 cd kafka_2.13-2.7.0
 
